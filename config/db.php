20953a5a7fe69db8b863059103c100c12ddf04a6
jabez.yong1215@gmail.com <Kelvin121595>
2017-08-16 10:21:24 +0800

    index

--- config/db.php
+++ config/db.php
@@ -6,8 +6,11 @@ return [
     //'username' => 'root',
     //'password' => 'kelvin121595',
     'dsn' => 'mysql:host=ap-cdbr-azure-southeast-b.cloudapp.net:3306;dbname=imoocshop',
+//    'dsn' => 'mysql:host=mysql.hostinger.co.uk;dbname=	u528800741_park',
     'username' => 'b2fd2deff82602',
     'password' => 'f8074fec',
+//        'username' => 'u528800741_park',
+//    'password' => '95770880',
     'charset' => 'utf8',
     'tablePrefix' => 'shop_',
 ];
--- config/web.php
+++ config/web.php
@@ -7,6 +7,8 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'defaultRoute' => 'index',
+    'index_page' => 'index.php',
+    'enable_query_strings' => TRUE,
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
